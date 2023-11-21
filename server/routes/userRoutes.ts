83e7f60c49bfa17a8b79fd8ecba61067368a073c
Michael Newbold <newboldm3@gmail.com>
2023-11-21 14:08:46 -0800

    Backend-tooling
    Co-authored-by: Jdave1125 <Jdave1125@users.noreply.github.com>

--- server/routes/userRoutes.ts
+++ server/routes/userRoutes.ts
@@ -3,50 +3,88 @@ import pool from '../database.ts';
 
 const userRoutes: Router = express.Router();
 
-userRoutes.post(
-  '/login',
-  async (req: Request, res: Response) => {
-    const { username}: { username: string} =
-      req.body;
+userRoutes.post('/login', async (req: Request, res: Response) => {
+  const { username }: { username: string } = req.body;
 
-    try {
-      const user = await pool.query('SELECT * FROM users WHERE username = $1', [
-        username,
-      ]);
-      // checking db for user
+  try {
+    const user = await pool.query('SELECT * FROM users WHERE username = $1', [
+      username,
+    ]);
+    // checking db for user
 
-      if (user.rows.length === 0) {
-        return res.status(401).json({ message: 'Authentication failed' });
-      } else {
-        res.json(user.rows[0]);
-        res.send('User logged in');
-      }
-    } catch (error) {
-      console.error('Error during login:', error);
-      res.status(500).json({ message: 'Internal Service Error' });
-    }
-  }
-);
+    if (user.rows.length === 0) {
+      return res.status(401).json({ message: 'Authentication failed' });
+    } else {
+      //inserting logged in user to the sessions db
+      const loggedInUser = user.rows[0];
 
-userRoutes.post(
-  '/signup',
-  async (req: Request, res: Response) => {
-    console.log('inside signup');
-    const { username, password }: { username: string, password: string } =
-      req.body;
-
-    try {
-      const user = await pool.query(
-        'INSERT INTO users (username, password) VALUES ($1, $2)',
-        [username, password]
+      const sessionResult = await pool.query(
+        'INSERT INTO sessions (user_id,session_state) VALUES ($1,$2) RETURNING *',
+        [loggedInUser.id, 'active']
       );
+      // creating cookie to go with session ID
+      const newSession = sessionResult.rows[0];
+      res.cookie('session_id', newSession.session_id, { httpOnly: true });
 
-      return res.json(user.rows[0]);
-    } catch (error) {
-      console.error('Error during signup: ', error);
-      res.status(500).json({ message: 'Internal Service Error' });
+      res.json(loggedInUser);
+      console.log('user logged in');
     }
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).json({ message: 'Internal Service Error' });
   }
-);
+});
 
+userRoutes.post('/signup', async (req: Request, res: Response) => {
+  console.log('inside signup');
+  const { username, password }: { username: string; password: string } =
+    req.body;
+
+  try {
+    const user = await pool.query(
+      'INSERT INTO users (username, password) VALUES ($1, $2)',
+      [username, password]
+    );
+
+    return res.json(user.rows[0]);
+  } catch (error) {
+    console.error('Error during signup: ', error);
+    res.status(500).json({ message: 'Internal Service Error' });
+  }
+});
+
+//render dashboard page based on session ID (session persistence)
+userRoutes.get('/dashboard', async (req: Request, res: Response) => {
+  const sessionId = req.cookies['session_id'];
+
+  if (!sessionId) {
+    return res.redirect('/login');
+  }
+
+  //we need to check the session id / session state of active
+  //if not active/not valid, return to login
+  try {
+    const session = await pool.query(
+      'SELECT * FROM sessions WHERE session_id = $1',
+      [sessionId]
+    );
+    if (
+      session.rows.length === 0 ||
+      session.rows[0].session_state !== 'active'
+    ) {
+      return res.redirect('/login');
+    }
+    //passes conditional? proceed
+    const user = await pool.query('SELECT * FROM users WHERE id=$1', [
+      session.rows[0].user_id,
+    ]);
+
+    res.render('dashboard', { user });
+  } catch (error) {
+    console.error('Error during dashboard route:', error);
+    res.status(500).json({ message: 'Internal Service Error' });
+  }
+
+  //else we want to render the dashboard with the user's session
+});
 export default userRoutes;
--- server/server.ts
+++ server/server.ts
@@ -1,39 +1,55 @@
-import express, { Request, Response } from 'express';
-import { ServerError } from './types';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { Request, Response, NextFunction } from 'express';
+import { ServerError } from '../types.ts';
 import path from 'path';
 import cors from 'cors';
 import userRoutes from './routes/userRoutes.ts';
+import sitRoutes from './routes/sitRoutes.ts';
 import 'dotenv/config';
+import cookieParser from 'cookie-parser';
 
 const PORT = process.env.PORT || 3030;
 const app = express();
 
 app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:3030',
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: 'http://localhost:3030',
+    credentials: true,
+  })
+);
 
+app.use(cookieParser());
 app.use('/users', userRoutes);
+app.use('/sits', sitRoutes);
 
 app.get('/tests', (_req, res) => {
-  console.log('get request successful')
-  res.send({text: 'Hi me'});
+  console.log('get request successful');
+  res.send({ text: 'Hi me' });
 });
 
-app.get('/', (_req: Request, res: Response): void => {
-  res.sendFile(path.resolve(__dirname, '../index.html'));
+app.use(express.static(path.join(path.resolve(), 'dist')));
+app.get('/', (req: Request, res: Response): void => {
+  const indexPath = path.join(path.resolve(), 'dist', 'index.html');
+  res.sendFile(indexPath);
 });
 
-app.use((err: ServerError, _req: Request, res: Response): Response => {
-  const defaultErr: ServerError = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 500,
-    message: { err: 'An error occured' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
-});
+app.use(
+  (
+    err: ServerError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Response => {
+    const defaultErr: ServerError = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 500,
+      message: { err: 'An error occured' },
+    };
+    const errorObj = Object.assign({}, defaultErr, err);
+    console.log(errorObj.log);
+    return res.status(errorObj.status).json(errorObj.message);
+  }
+);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
